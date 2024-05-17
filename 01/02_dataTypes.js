a70a3b8ca19c88f50c7703c5a7b9793aae0903da
vikeshpoojary <vrp6000@gmail.com>
2024-05-17 18:59:34 +0530

    Create 02_dataTypes.js
    datatype

---
+++ 01/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict";//treat all code as new JS version, but nowdays it is not required as console automatically runs it in new version
+//alert(3+3) //we are using nodeJS not browser
+console.log(3+3);
+console.log("Vikesh");//code redeablity should be good
+console.log(typeof null); //null is object 
+console.log(typeof undefined);
+console.log(typeof true);
